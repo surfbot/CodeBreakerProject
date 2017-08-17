a60774ca33e2cceec9f879d79f202a4d911c809c
sswart5 <swartz@gmx.com>
2017-08-17 16:11:19 -0500

    latest

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,41 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
+let message = document.getElementById('message');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+
+    if(attempt === '' || answer === '') {
+        setHiddenFields();
+    }
+
+    if(!validateInput(input.value)) {
+        return false;
+    } else {
+        attempt++;
+    }
+
+    // scope, wrap all within guess()?
 }
 
-//implement new functions here
+function setHiddenFields() {
+    attempt = 0;
+    answer = math.floor(math.random() * 10000).toString();
+
+    while(answer.length < 4) {
+        answer = 0 + answer;
+    }
+}
+
+function setMessage(text) {
+    message = text.innerHTML;
+}
+
+function validateInput(input) {
+    if(input.length === 4) {
+        return true;
+    } else {
+        setMessage('Guesses must be exactly 4 characters long.');
+        return false;
+    }
+}
